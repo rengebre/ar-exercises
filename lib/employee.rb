9b387ed82509e09cb980b06eef907ad0610a08db
Russell Engebretson <russell.engebretson@gmail.com>
2021-11-09 19:13:43 +0000

    add validators and testing for them

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,9 @@
 class Employee < ActiveRecord::Base
+  validates :first_name, :last_name, :store, presence: true
+
+  validates :hourly_rate, numericality: {greater_than_or_equal_to: 40}
+
+  validates :hourly_rate, numericality: {less_than_or_equal_to: 200}
+
   belongs_to :store
 end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,15 @@
 class Store < ActiveRecord::Base
+  validates :name, length: { minimum: 3 }
+
+  validates :annual_revenue, numericality: { greater_than_or_equal_to: 0}
+
+  validate :must_carry_mens_or_womens_apparel
+  
   has_many :employees
+
+  def must_carry_mens_or_womens_apparel
+    if !(mens_apparel || womens_apparel)
+      errors.add(:base, "must sell either mens or womens apparel")
+    end 
+  end
 end
