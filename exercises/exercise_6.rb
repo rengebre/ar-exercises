d6b7ecfe7854ef518d63fedcb1b73142f39eaa85
Russell Engebretson <russell.engebretson@gmail.com>
2021-11-09 18:07:50 +0000

    complete exercise 6, creating employee elements

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,12 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Katie", last_name: "Herda", hourly_rate: 70)
+@store1.employees.create(first_name: "Vanessa", last_name: "Goldgrub", hourly_rate: 115)
+@store1.employees.create(first_name: "Sean", last_name: "Hayes", hourly_rate: 100)
+
+@store2.employees.create(first_name: "Sophie", last_name: "Oliver", hourly_rate: 119)
+@store2.employees.create(first_name: "Russell", last_name: "Engebretson", hourly_rate: 120)
+@store2.employees.create(first_name: "Laura", last_name: "Brett", hourly_rate: 10)
+@store2.employees.create(first_name: "Rachel", last_name: "Goldgrub", hourly_rate: 114)
